e239e08e0106ab307962eba9ab52808c75f6028a
Brad Borodaty <brad.borodaty@gmail.com>
2021-07-12 12:04:06 -0700

    add mobile styling

--- src/components/BlackJackHand/BlackJackHand.jsx
+++ src/components/BlackJackHand/BlackJackHand.jsx
@@ -128,41 +128,43 @@ const BlackJackHand = (props) => {
                 {/* spacer */}
                 <div className={styles.HandSpacer}></div>
                 {/* player */}
-                <div className={styles.ContainerHand}>
-                <div className={styles.ScoreContainer}>
-                    <div className={styles.Score}>
-                        <div>{state.player.score}</div>
-                        <div></div>
-                    </div>
-                </div>
-                <div className={styles.Cards}>
-                    {state.player.cards.map((card, index) => (
-                        <div key={card.code} className={styles.Card}>
-                            <PlayingCard card={card} />
+                <div className={styles.ContainerHandMobile}>
+                    <div className={styles.PlayerHandContainer}>
+                        <div className={styles.ScoreContainer}>
+                            <div className={styles.Score}>
+                                <div>{state.player.score}</div>
+                                <div></div>
+                            </div>
                         </div>
-                    ))}
-                </div>
-                <div className={styles.MenuContainer}>
-                    <div className={styles.PlayerTitle}>Player 1</div>
-                    <div>
-                        <Button
-                            onClick={() => dispatch(hit())}
-                            disabled={state.player.isFinished}
-                            classes={styles.HitButton}
-                        >
-                            Hit
-                        </Button>
-                        <div className={styles.Spacer}></div>
-                        <Button
-                            onClick={() => dispatch(stay())}
-                            disabled={state.player.isFinished}
-                            classes={styles.StayButton}
-                        >
-                            Stay
-                        </Button>
+                        <div className={styles.Cards}>
+                            {state.player.cards.map((card, index) => (
+                                <div key={card.code} className={styles.Card}>
+                                    <PlayingCard card={card} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={styles.MenuContainer}>
+                        <div className={styles.PlayerTitle}>Player 1</div>
+                        <div>
+                            <Button
+                                onClick={() => dispatch(hit())}
+                                disabled={state.player.isFinished}
+                                classes={styles.HitButton}
+                            >
+                                Hit
+                            </Button>
+                            <div className={styles.Spacer}></div>
+                            <Button
+                                onClick={() => dispatch(stay())}
+                                disabled={state.player.isFinished}
+                                classes={styles.StayButton}
+                            >
+                                Stay
+                            </Button>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     );
